9fe45d6129db92bb89c3597776c01cb2bb366780
nicholas blackburn <nickblackburn02@gmail.com>
2020-03-19 10:13:40 -0400

    added mod id

--- src/main/java/space/nicholasblackburn/crystalgems/Main.java
+++ src/main/java/space/nicholasblackburn/crystalgems/Main.java
@@ -1,8 +1,17 @@
 package space.nicholasblackburn.crystalgems;
 
+import java.util.logging.LogManager;
+
+import net.minecraftforge.fml.Logging;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("crystalgems")
-public class Main{
+
+public class Main {
+
+    private static final LogManager Logger = LogManager.getLogManager();
+
+    public static final String MODID = "crystalgems";
+
 
 }
